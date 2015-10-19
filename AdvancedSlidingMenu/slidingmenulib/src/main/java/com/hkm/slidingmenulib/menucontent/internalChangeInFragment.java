109ad38cbaa28c9e65f8c81d0f6fe91fd79e6d2d
世外桃源 <jobhesk@gmail.com>
2015-10-19 10:52:56 +0800

    V23 test and upload
    change the library to support API V23

--- AdvancedSlidingMenu/slidingmenulib/src/main/java/com/hkm/slidingmenulib/menucontent/internalChangeInFragment.java
+++ AdvancedSlidingMenu/slidingmenulib/src/main/java/com/hkm/slidingmenulib/menucontent/internalChangeInFragment.java
@@ -5,10 +5,9 @@ package com.hkm.slidingmenulib.menucontent;
  * Created by hesk on 23/6/15.
  */
 public interface internalChangeInFragment<Fragment> {
-    public void setinternalChange(Fragment section, String title);
+    void setinternalChange(Fragment section, String title);
 
-    public void setinternalChange(Fragment section, String title, Fragment previousFragment, boolean closedrawer);
+    void setinternalChange(Fragment section, String title, Fragment previousFragment, boolean closedrawer);
 
-    public void setinternalChange(Fragment section, String title, Fragment previousFragment);
-
+    void setinternalChange(Fragment section, String title, Fragment previousFragment);
 }
